51a58879ece47eb7271db035cdeb8473da5e5f8e
abinayaarjunan <abiharsha2000@gmail.com>
2023-08-11 12:28:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 //component class r function
 //rfce
-const App=() => {
+// const App=() => {
+// //javascript
+//   console.log("hello from component");
+//   //jsx js+xml
+//   return (
+//     <div >
+//       <p>Hello World!</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+const Hello = (props) => {
+  return (
+    <p>hello {props.name} and {props.age}!</p>
+  )
+  
+}
+
+const App = () => {
 //javascript
   console.log("hello from component");
   //jsx js+xml
+  let age = 10;
   return (
     <div >
-      <p>Hello World!</p>
+      <p><Hello name="krish" age={age }/></p>
     </div>
   );
 }
 
-export default App;
